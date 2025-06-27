9b562104f8dc083924a6aba49d812dc37af28a8d
madhavguptaa <madhav87308@gmail.com>
2025-06-28 02:08:06 +0530

    base url added

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,10 +1,6 @@
-
-
 const host = window.location.hostname;
 
-export const API_BASE_URL =
-  host === "localhost" || host === "127.0.0.1"
+export const API_BASE_URL = 
+  host === "localhost" || host === "127.0.0.1" || host === "accounts.google.com"
     ? "http://127.0.0.1:8000" // Local Django backend
-    : "http://13.218.59.50:8000"; 
-
-export default API_BASE_URL;
+    : "http://13.218.59.50:8000"; // Production backend
